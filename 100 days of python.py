70ebc0cb792795f932f6a60d77d5fbd2fa74d5d8
shivakumarhavaldar2-svg <shivakumarhavaldar2@gmail.com>
2026-06-14 20:40:52 +0530

    Add files via upload

---
+++ 100 days of python.py
@@ -0,0 +1,20 @@
+#DAY - 1 ALL THE PRACTICE TASKS ------
+#print("Hello world")
+#print("Hello world\nHello world\nHello world")
+#print ("Hello " + "Shiva")
+# print("Hello" + " " + "Shiva")
+#print("Hello " + input("What is you name?") + "!")
+#name = " Shivakumar "
+#name = input("What is your name? ")
+#print(name)
+#print(len(name))
+#print(len(input("What is your name? ")))
+#Username = input('Please enter your username: ')
+#Length = len(Username)
+#print(Length)
+
+#Project-1 of 100 days of python
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the pet name?\n")
+print("Your band name could be: " + city + " " + pet)
